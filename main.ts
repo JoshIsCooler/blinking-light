1ab8389356562fe3b85558f11e5d6f443ced1a69
Josh <98833285+JoshIsCooler@users.noreply.github.com>
2022-02-02 11:08:08 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,24 @@
+basic.forever(function () {
+    basic.showLeds(`
+        # . . . #
+        # . . . .
+        # # # . #
+        # . # . #
+        # . # . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . . . #
+        # . . . .
+        # # # . #
+        # . # . #
+        # . # . #
+        `)
+})
+
